d63bb07b21caf128071ff2489b791351a3b72b86
Tal Mashiah <talmashiah@gmail.com>
2020-01-18 19:04:06 +0200

    add text form

--- frontend/src/services/BoardService.js
+++ frontend/src/services/BoardService.js
@@ -1,33 +1,10 @@
 import UtilsService from './UtilsService.js'
 import HttpService from './HttpService';
-let gBoard = {
-    _id: UtilsService.makeRandomId(),
-    members: ['tal', 'shmuel,gilad'],
-    topics: [{
-        id: UtilsService.makeRandomId(),
-        title: 'Things to do',
-        tasks: [{ title: 'blabla' }, { title: 'hello' }, { title: 'blabla' }]
-    },
-    {
-        id: UtilsService.makeRandomId(),
-        title: 'Doing',
-        tasks: [{ title: 'blabasdasdasla' }, { title: 'hello' }, { title: 'blabla' }]
-    },
-    {
-        id: UtilsService.makeRandomId(),
-        title: 'Done',
-        tasks: [{ title: 'blabla' }, { title: 'helasdadlo' }, { title: 'blablasdasda' }]
-    }]
-
-};
-
 
 async function query() {
  return await  HttpService.get('board')
   
 }
-
-
 
 export default {
     query
