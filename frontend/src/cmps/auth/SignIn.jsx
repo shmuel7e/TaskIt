41255a6d3ab5aff33577de172cdce8b1ef190cbd
gilad <giladberg@gmail.com>
2020-01-19 10:25:09 +0200

    dsf

--- frontend/src/cmps/auth/SignIn.jsx
+++ frontend/src/cmps/auth/SignIn.jsx
@@ -1,20 +1,32 @@
 import React, { Component } from 'react'
 
 export default class SignIn extends Component {
+    state = { credentials: { email: '', password: '' } }
+
+    inputChange =(ev) => {
+        let value = ev.target.value
+        let fieldName = ev.target.name
+        const credentials = this.state.credentials
+        credentials[fieldName] = value
+        this.setState({ credentials })
+    }
+    checkCredentials=()=>{
+        console.log(this.state.credentials)
+    }
     render() {
         return (
             <div className="form sign-in">
                     <h2>Welcome back,</h2>
                     <label className='form-label'>
                         <span className='form-span'>Email</span>
-                        <input type="email" className='form-input'/>
+                        <input onChange={this.inputChange} name="email" type="email" className='form-input'/>
                     </label>
                     <label className='form-label'>
                         <span className='form-span'>Password</span>
-                        <input type="password" className='form-input'/>
+                        <input onChange={this.inputChange} name="password"  type="password" className='form-input'/>
                     </label>
                     <p className="forgot-pass">Forgot password?</p>
-                    <button type="button" className="submit form-button">Sign In</button>
+                    <button onClick={this.checkCredentials} type="button" className="submit form-button">Sign In</button>
                     <button type="button" className="fb-btn form-button">Connect with <span className='form-span'>facebook</span></button>
                 </div>
         )
