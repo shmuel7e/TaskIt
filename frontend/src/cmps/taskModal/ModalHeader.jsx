f544e2adb57132bf1e8c7f0a9e87314de884f9a0
Tal Mashiah <talmashiah@gmail.com>
2020-01-24 17:22:31 +0200

    txt fix

--- frontend/src/cmps/taskModal/ModalHeader.jsx
+++ frontend/src/cmps/taskModal/ModalHeader.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class ModalHeader extends Component {
 
     onTxtChange = (editedTxt) => {
-        this.props.changeTaskTitle(this.props.topic,this.props.task,editedTxt)
+        this.props.changeTaskTitle(this.props.topic, this.props.task, editedTxt)
     }
 
     onCloseModal = () => {
@@ -17,8 +17,10 @@ export default class ModalHeader extends Component {
             <div className="modal-header">
                 <span className='icon-cross' onClick={this.onCloseModal}></span>
                 <span className="icon-window"></span>
-                <div className="modal-txt title" style={{ background: task.bgColor}} suppressContentEditableWarning={true} contentEditable="true" onBlur={(e) => this.onTxtChange(e.target.textContent)}>{task.title}</div>
-                <small>In list <span className="bottom-line">{topic.title}</span></small>
+                <div className="modal-txt title" style={{ background: task.bgColor }} suppressContentEditableWarning={true} contentEditable="true" onBlur={(e) => this.onTxtChange(e.target.textContent)}>{task.title}</div>
+                <div className="small-title">
+                    <small>In list <span className="bottom-line">{topic.title}</span></small>
+                </div>
             </div>
         )
     }
