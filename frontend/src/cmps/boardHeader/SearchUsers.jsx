0b876c6ddb0985e9597a92e1a9efff4c16ec9438
Tal Mashiah <talmashiah@gmail.com>
2020-01-25 15:21:15 +0200

    invite

--- frontend/src/cmps/boardHeader/SearchUsers.jsx
+++ frontend/src/cmps/boardHeader/SearchUsers.jsx
@@ -4,14 +4,24 @@ import UserList from './UserList.jsx'
 
 
 export default class SearchUsers extends Component {
+
+    onCloseModal = () => {
+        this.props.toggleUsersModal();
+    }
+
     render() {
         return (
-            <div className="flex column justify-center">
-                <Filter onSearchUsers={this.props.onSearchUsers} />
-                <UserList
-                    onAddMember={this.props.onAddMember}
-                    membersToInvite={this.props.membersToInvite}
-                />
+            <div className="search-users-modal">
+                <span onClick={this.onCloseModal} className="icon-cross"></span>
+                <h3>Invite To Board</h3>
+                <div className="search-users-content">
+                    <Filter onSearchUsers={this.props.onSearchUsers} />
+                    <UserList
+                        onAddMember={this.props.onAddMember}
+                        membersToInvite={this.props.membersToInvite}
+                        toggleUsersModal={this.props.toggleUsersModal}
+                    />
+                </div>
             </div>
         )
     }
