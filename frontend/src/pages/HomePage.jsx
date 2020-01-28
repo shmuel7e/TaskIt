a15836d8695c2d2c058434100fd19d7f39f35c16
shmuelelkis <shmuel7e@gmail.com>
2020-01-28 22:02:05 +0200

    responsive changes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,24 +1,8 @@
 import React, { Component } from 'react';
-import TemplateList from '../cmps/template/TemplateList.jsx';
-import TemplateService from '../services/TemplateService.js';
 import Footer from '../cmps/footer/Footer.jsx';
 
 
 export default class HomePage extends Component {
-
-
-    state = {
-        templates: [],
-    }
-
-    componentDidMount() {
-        this.getGalleryTemplates();
-    }
-
-    getGalleryTemplates = async () => {
-        const templates = await TemplateService.getGalleryTemplates();
-        await this.setState({ templates });
-    }
 
     render() {
         return (
@@ -31,13 +15,10 @@ export default class HomePage extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="home-page-templates">
-                    <TemplateList type={'top'} templates={this.state.templates} />
-                </div>
-                <div className="home-page-section">
+                {/* <div className="home-page-section">
                     <h1 className="home-section-title">Work together, and acheive more.</h1>
                     <h2 className="home-section-desc">Use Taskit the way your team works best. We’ve got the flexibility & features to fit any team’s style.</h2>
-                </div>
+                </div> */}
                 <Footer />
             </div>
         )
