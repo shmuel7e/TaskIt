77a3528dab9b04d5b5e359be35b1868419e3e74a
gilad <giladberg@gmail.com>
2020-01-29 23:50:52 +0200

    more push

--- frontend/src/cmps/boardHeader/BoardMemberPreview.jsx
+++ frontend/src/cmps/boardHeader/BoardMemberPreview.jsx
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 import UtilsService from '../../services/UtilsService';
 
 export default class BoardMemberPreview extends Component {
-
+    state = { isModalShown: false }
     onGetInitials = (fullName) => {
         return UtilsService.getInitials(fullName);
+    }
+    isModalShown = () => {
+        let isModalShown = !this.state.isModalShown
+        this.setState({ isModalShown })
     }
 
     render() {
         const { member } = this.props
         return (
-            <div>
-                <div className="member-name-initials"
+            <div className="boardMember-container">
+                <div onClick={this.isModalShown} className="member-name-initials"
                     style={{ background: member.bgColor }}
                     data-toggle="tooltip" title={member.username}>
-                    {this.onGetInitials(member.username)}</div>
+                    {this.onGetInitials(member.username)}
+                    {this.state.isModalShown
+                    ? <div className="member-mini-menu">
+                        <div >Delete</div>
+                    </div> : ''}
+                </div>
+                
             </div>
         )
     }
