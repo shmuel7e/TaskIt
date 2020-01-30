ca03ea4ea565a7f30434daabfd1c89e3d9f88b9d
gilad <giladberg@gmail.com>
2020-01-30 21:43:22 +0200

    aa

--- backend/public/service-worker.js
+++ backend/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c084060ba076a9fae12d358669065ecd.js"
+  "/precache-manifest.36a75d7dfcb13602345f0a4167f6c57f.js"
 );
 
 self.addEventListener('message', (event) => {
