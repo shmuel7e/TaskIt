5d27994f0f0e00902ef7f85e0211194bc409b6fc
gilad <giladberg@gmail.com>
2020-02-01 17:30:15 +0200

    for production

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,8 +3,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
+const composeEnhancers =  compose;
+//window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
 const Store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(thunk))
